43d6d1b0f19bf28a0cd92926892ee83af448579a
Josh <vansjo01@luther.edu>
2020-11-02 22:32:28 -0600

    added whitenoise to settings middleware

--- backend/settings.py
+++ backend/settings.py
@@ -57,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 AUTH_USER_MODEL = 'main.User'
