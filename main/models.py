aa7bc9ff56f89874d3bc188029bd5652e696d04a
David Will <willda07@luther.edu>
2020-11-02 20:05:45 -0600

    Add secure login w/Django

--- main/models.py
+++ main/models.py
@@ -2,25 +2,11 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser, Group
 
 class User(AbstractUser):
-    picture=models.CharField(max_length=100, default='pic1')
-    theme=models.CharField(max_length=100, default='theme1')
-    phone=models.IntegerField(default=9119119111)
+    picture = models.CharField(max_length=100, default='pic1')
+    theme = models.CharField(max_length=100, default='theme1')
+    phone = models.IntegerField(default=9119119111)
 
 
 class MyGroup(Group):
-    picture=models.CharField(max_length=100, default='pic1')
-    announcement=models.CharField(max_length=100, default='This is a group')
-
-
-    
-# class User(models.Model):
-#     firstName= models.CharField(max_length=100)
-#     lastName= models.CharField(max_length=100)
-#     picture=models.CharField(max_length=100)
-#     theme=models.CharField(max_length=100)
-#     phone=models.IntegerField()
-#     email = models.EmailField(max_length=100, unique=True)
-#     group = models.ManyToManyField(Group)
-
-#     def __str__(self):
-#         return self.name
+    picture = models.CharField(max_length=100, default='pic1')
+    announcement = models.CharField(max_length=100, default='This is a group')
